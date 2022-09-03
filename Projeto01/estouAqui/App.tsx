573be7ba63d3fc8486711319107fcc1798963bc5
marlonballa <marlonballa07@gmail.com>
2022-09-03 17:55:52 -0300

    Trilha de React Native - Projeto 01: Fim da Etapas de Fundamentos

--- Projeto01/estouAqui/App.tsx
+++ Projeto01/estouAqui/App.tsx
@@ -1,12 +1,19 @@
 //Como criamos um elemento externo, devemos realizar sua importação para dentro deste arquivo. Pois este arquivo será o responsável por renderizar os conteúdos na tela. 
-
 import Home from './src/screens/Home';
-
-
+//Importando componentes do React Native
+import { StatusBar } from 'react-native';
 //Único momento que usaremos a exportação default, pois é a inicialização do aplicação. 
 export default function App() {
     return (
-        <Home />
+        <>
+        {/**Podemos estilizar a StatusBar (barra de status com icones que apresentam informações e ficam na parte superior da aplicação)*/}
+            <StatusBar
+                barStyle={'light-content'}
+                backgroundColor="transparent"
+                translucent
+            />
+            <Home />
+        </>
     )
 }
 
--- Projeto01/estouAqui/src/screens/Home/index.tsx
+++ Projeto01/estouAqui/src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 //O Arquivo .tsx é o responsável pela estrutura do componente. 
 
 //Para utilizar elementos, precisamos importar eles na biblioteca. 
-import { Text, View, TextInput, TouchableOpacity, ScrollView} from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, ScrollView, Alert} from 'react-native';
 //Importanto a folha de estilos
 import { styles } from './styles'
 //Importando o componente Participante
@@ -15,10 +15,23 @@ const participants = ["Marlon", "Ana Laura", "Marcelle", "Sandra", "Sebastião",
   //Toda função possui um retorno. Aqui vamos retornar os elementos que queremos que apareçam na tela.
   //Todas as funções devem ser criadas dentro do escopo do componente.
   function handleParticipantAdd() {
-    console.log("Clicou em adicionar")
+    if(participants.includes("Marlon")) {
+      return Alert.alert("Eita, eita, eita!", "Já existe um participante na lista com este nome")
+      /**Ao adicionarmos o return, ele para a execução da função */
+    }
   }
 
   function handleParticipantRemove(nameUser: string) {
+    Alert.alert("1,2,3... Já era!", `Deseja mesmo remover ${nameUser}?`, [
+      {
+        text: "Quero",
+        onPress: () => Alert.alert("Esse já era!")
+      }, 
+      {
+        text: "Pensei melhor",
+        style: "cancel"
+      }
+    ])
     console.log(`Clicou em remover o participante: ${nameUser}`)
   }
 
@@ -36,7 +49,6 @@ const participants = ["Marlon", "Ana Laura", "Marcelle", "Sandra", "Sebastião",
               style={styles.input}
               placeholder="Digite"
               placeholderTextColor="#b0b3b8"
-              keyboardType="numeric"
           />
           {/*Criando um botão. *Elementos de texto devem sempre estar encapsulados por Text*/}
           <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
@@ -45,7 +57,12 @@ const participants = ["Marlon", "Ana Laura", "Marcelle", "Sandra", "Sebastião",
             </Text>
           </TouchableOpacity>
         </View>
-        {/**A ScrollView permite a rolagem dentro dela e o atributo showsVerticalScrollIndicator ativa ou desativa a visualização da barra de rolagem */}
+        {/**A ScrollView permite a rolagem dentro dela e o atributo showsVerticalScrollIndicator ativa ou desativa a visualização da barra de rolagem. 
+         * Uma alternativa para visualizar listas é o FlatList
+         * Principal diferença entre as duas:
+         * A ScrollView carrega todos os componentes que irão compor a lista, enquanto a FlatList carrega apenas os componentes que estão visíveis na tela. 
+         * Ou seja, a FlatList perfoma muito mais do que a ScrollView e por isso é indicada para trabalhar com uma grande quantidade de dados. Afinal, carregar apenas o essencial deixa a aplicação mais leve. 
+         */}
         <ScrollView showsVerticalScrollIndicator={false}>
           {
             /**Quando utilizamos uma estrutura de repetição para gerar vvários componentes,devemos utilizar uma chave para que o React entenda que cada elemento é único. Utilizamos a propriedade Key para fazer isso.*/
@@ -53,7 +70,7 @@ const participants = ["Marlon", "Ana Laura", "Marcelle", "Sandra", "Sebastião",
               <Participant
                 key={participant}
                 nameUser={participant}
-                onRemove={() => handleParticipantRemove("Marlon")} />
+                onRemove={() => handleParticipantRemove(participant)} />
               /*Criamos uma propriedade que usará uma function que está neste arquivo, mas será disparada quando o usuário clicar no componente (que está em outro lugar)*/
             ))
           }
