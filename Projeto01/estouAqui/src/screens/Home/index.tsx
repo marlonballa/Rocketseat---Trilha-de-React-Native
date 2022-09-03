d694273d7eed647c4ee3c86a8558cce0ae1c8dba
marlonballa <marlonballa07@gmail.com>
2022-09-03 17:15:26 -0300

    Trilha de React Native - Projeto01: O componente ScrollView

--- Projeto01/estouAqui/src/screens/Home/index.tsx
+++ Projeto01/estouAqui/src/screens/Home/index.tsx
@@ -1,14 +1,17 @@
 //O Arquivo .tsx é o responsável pela estrutura do componente. 
 
 //Para utilizar elementos, precisamos importar eles na biblioteca. 
-import { Text, View, TextInput, TouchableOpacity} from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, ScrollView} from 'react-native';
 //Importanto a folha de estilos
 import { styles } from './styles'
 //Importando o componente Participante
 import { Participant } from '../../components/Participant';
 
-//A função App é a padrão para a inicialização da aplicação.
-export default function Home() {
+//A função App é a padrão para a inicialização
+export default function App() {
+//Responsável por armazenar os participantes
+const participants = ["Marlon", "Ana Laura", "Marcelle", "Sandra", "Sebastião", "Welinton", "Paulo", "Camila", "Itaide Balla", "Jesus Balla", "Marcelo", "Leandro", "Reginaldo", "Cislene", "Clara", "Toquinho", "Estéfano"]
+
   //Toda função possui um retorno. Aqui vamos retornar os elementos que queremos que apareçam na tela.
   //Todas as funções devem ser criadas dentro do escopo do componente.
   function handleParticipantAdd() {
@@ -42,8 +45,19 @@ export default function Home() {
             </Text>
           </TouchableOpacity>
         </View>
-        <Participant nameUser="Rodrigo faro" onRemove={() => handleParticipantRemove("Marlon")} />
-          {/*Criamos uma propriedade que usará uma function que está neste arquivo, mas será disparada quando o usuário clicar no componente (que está em outro lugar)*/}
+        {/**A ScrollView permite a rolagem dentro dela e o atributo showsVerticalScrollIndicator ativa ou desativa a visualização da barra de rolagem */}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {
+            /**Quando utilizamos uma estrutura de repetição para gerar vvários componentes,devemos utilizar uma chave para que o React entenda que cada elemento é único. Utilizamos a propriedade Key para fazer isso.*/
+            participants.map(participant => (
+              <Participant
+                key={participant}
+                nameUser={participant}
+                onRemove={() => handleParticipantRemove("Marlon")} />
+              /*Criamos uma propriedade que usará uma function que está neste arquivo, mas será disparada quando o usuário clicar no componente (que está em outro lugar)*/
+            ))
+          }
+      </ScrollView>
     </View>
     //Quando o retorno for mais do que um elemento, o conteúdo deve estar dentro de uma View (equivalente a uma div).
     // <> </> Fragement: utilizado para embrulhar elementos. 
